f6e05108ee4df6db02189ef18598c59fe37bb464
DanielCarro20 <danie@Daniel>
2025-11-17 12:48:30 +0100

    hola

--- RetosDanielSergio/src/reto/funciones2.java
+++ RetosDanielSergio/src/reto/funciones2.java
@@ -2,4 +2,33 @@ package reto;
 
 public class funciones2 {
 
+	
+	public static int dividir(int a, int b) {
+		b = 0;
+		if (b==0) {
+			return 0;
+		}
+		return a/b;
+	}
+	
+	
+	public static int multiplicar (int a, int b) {
+		return a*b;
+	}
+	
+	
+	public static boolean esPositivo(int n) {
+		if (n >= 0) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static boolean esNegativo(int n) {
+		if (n < 0) {
+			return true;
+		}
+		return false;
+	}
 }
+
