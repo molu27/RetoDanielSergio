a7dfc847339798f96170ba2f8b36ff95fd507d68
molu27 <1DAM@A11-PC25>
2025-11-17 12:07:53 +0100

    Hola, soy Sergio Sarasa y he hecho funciones1, sumar, restar, esPar y esImpar

--- RetosDanielSergio/src/reto/funciones1.java
+++ RetosDanielSergio/src/reto/funciones1.java
@@ -1,5 +1,6 @@
 package reto;
 
+public class funciones1 {
 public static int sumar(int n1, int n2) {
 	return n1 + n2;
 	
@@ -7,13 +8,19 @@ public static int sumar(int n1, int n2) {
 public static int restar(int n1, int n2) {
 	return n1 - n2;
 }
-public static int esPar (int n1) {
+public static boolean esPar (int n1) {
 	if (n1 % 2 == 0) {
-		return esPar;
+		return true;
 	}
-		return esImpar;
+		return false;
+}
+public static boolean esImpar (int n1) {
+	if (n1 % 2 != 0) {
+		return true;
+	}
+		return false;
 }
 
-public class funciones1 {
+
 }
 
--- RetosDanielSergio/src/reto/reto1.java
+++ RetosDanielSergio/src/reto/reto1.java
@@ -1,7 +1,5 @@
 package reto;
 
-import util.funciones
-
 public class reto1 {
 
 	public static void main(String[] args) {
